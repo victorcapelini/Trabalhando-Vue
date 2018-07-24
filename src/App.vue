52bbc8ee3de86b5a507c468ae64511f53b54b579
Victor <victorcapelini@hotmail.com>
2018-07-24 16:52:01 -0300

    Criado novo componente que cria os paineis para as imagens.

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,63 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h1>{{ nome }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div class="corpo">
+  <h1 class="centralizado">{{titulo}}</h1>
+  
+  <ul class="lista-fotos">
+    <li class="lista-fotos-item" v-for="foto of fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
-  name: 'app',
-  data () {
+  components: {
+    "meu-painel": Painel
+  },
+
+  data() {
     return {
-      msg: 'Bem Vindo ao Vue!',
-      nome: 'Muahaha'
-    }
+      titulo: "Imagens aleatórias",
+      fotos: []
+    };
+  },
+
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
+.lista-fotos {
+  list-style: none;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
+.lista-fotos .lista-fotos-item {
   display: inline-block;
-  margin: 0 10px;
 }
 
-a {
-  color: #42b983;
+.imagem-responsiva {
+  width: 100%;
 }
 </style>
